f8d54ad97a2a390123c79870557b88fcd6e59295
Saurav Kumar Mahato <077bct079.saurav@pcampus.edu.np>
2022-06-09 08:44:33 +0545

    qn5 file made

---
+++ qn5.cpp
@@ -0,0 +1,33 @@
+/*
+qn5.cpp
+
+Assume that an object represents an employee report that contains information like employee id, total bonus, total overtime in a particular 
+year. Use an array of objects to represent n employees' reports. Write a program that displays the report. Use setpara() member function to
+set report attributes by passing the arguments and member function displayreport() to show the report according to the parameter 
+passed. Display the report in the following format.
+
+An employee with ... ... ... has received Rs ... ... ...as a bonus
+and
+had worked ... ... ... hours as overtime in the year ... ... ...
+*/
+
+#include <iostream>
+
+class EmployeeReport
+{
+private:
+    int employee_id{};
+    int total_bonus{};
+    int total_overtime{};
+
+public:
+    
+};
+
+
+int main()
+{
+
+
+    return 0;
+}
