a2594107c12fb428d851458361987c27bb7ad0fd
Saurav Kumar Mahato <sauravmahato139@gmail.com>
2022-07-17 23:49:25 +0545

    made assignment folder and done some programs

---
+++ assignment_classroom/New/delete operator.cpp
@@ -0,0 +1,41 @@
+/*
+New/delete operator
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+class Student
+{
+    string name;
+    int age;
+
+public:
+    Student(string name = " ", int age = 0)
+    {
+        std::cout<<"\nConstructor is called\n";
+        this->name = name;
+        this->age = age;
+    }
+    string get_name() { return name; }
+    int get_age() { return age; }
+
+    void *operator new(size_t size)
+    {
+        std::cout << "\nNew is called and size is: " << size;
+        void *temp = malloc(size);
+        return temp;
+    }
+};
+
+int main()
+{
+
+    Student *std;
+    std = new Student("Anonyms", 100);
+    cout << "Name is : " << (*std).get_name() << "\nAge is: " << (*std).get_age() << endl;
+    return 0;
+}
