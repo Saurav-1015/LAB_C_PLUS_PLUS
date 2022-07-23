f174cf894b3f9014df3b6876783410754fe6d137
Saurav Kumar Mahato <sauravmahato139@gmail.com>
2022-07-24 00:32:41 +0545

    lab8 folder created and wrote some theroy portion as note: :

---
+++ lab8/qn1.cpp
@@ -0,0 +1,17 @@
+/*
+Write a program to demonstrate the use of different ios 
+flags and functions to format the output. Create a program to generate
+ the bill invoice of a department store by using different formatting.
+ */
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    
+
+
+return 0;
+}
