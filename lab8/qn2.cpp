9880293b350b9f64d7362a9c0167398aad10c556
Saurav Kumar Mahato <sauravmahato139@gmail.com>
2022-07-25 00:07:51 +0545

    qn2.cpp done of lab8

--- lab8/qn2.cpp
+++ lab8/qn2.cpp
@@ -5,12 +5,35 @@ same time by passing arguments.
 */
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+class Manip
+{
+    private: 
+    int width, precision;
+    char fill;
+    
+    public:
+    Manip (int w, int p , char f ): width(w) , precision(p), fill(f) {}
+    friend ostream& operator<<( ostream& ,Manip m );
+};
 
+  ostream& operator<<( ostream&os ,Manip m)
+    {
+        os.precision(m.precision);
+        os << setw(m.width);
+        os.fill(m.fill);
+        return os;
+    }
 
+    Manip wpf(int w, int p, char f)
+    {
+        return Manip( w, p, f);
+    }
 int main()
 {
-
+    float x = 345.458;
+    cout << wpf(9,4,'#')<< x;
 return 0;
 }
