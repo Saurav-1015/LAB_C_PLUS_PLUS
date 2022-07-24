d66d59541da87698aafee91ae02e1b7df8ccd614
Saurav Kumar Mahato <sauravmahato139@gmail.com>
2022-07-24 23:14:51 +0545

    some changes done in qn2.cpp of lab8

---
+++ lab8/qn2.cpp
@@ -0,0 +1,16 @@
+/*
+Write a program to create a user-defined manipulator that will format 
+the output by setting the width, precision, and fill character at the 
+same time by passing arguments.
+*/
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+return 0;
+}
